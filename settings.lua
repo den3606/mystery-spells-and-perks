a88f63ba4540394daca26bc8e01a7b30730cf416
den3606 <dedendendedenpunn@gmail.com>
2024-06-20 20:29:56 +0900

    feat: localized for jp on setting page

--- settings.lua
+++ settings.lua
@@ -3,11 +3,186 @@ dofile("data/scripts/lib/mod_settings.lua") -- see this file for documentation o
 local mod_id = "mystery_spells_and_perks"   -- This should match the name of your mod's folder.
 mod_settings_version = 5                    -- This is a magic global that can be used to migrate settings to new mod versions. call mod_settings_get_version() before mod_settings_update() to get the old value.
 
+local function language()
+  local current_language = GameTextGet("$current_language")
+  if current_language == "English" then
+    return 'en'
+  end
+  if current_language == "русский" then
+    return 'ru'
+  end
+  if current_language == "Português (Brasil)" then
+    return 'pt-br'
+  end
+  if current_language == "Español" then
+    return 'es-es'
+  end
+  if current_language == "Deutsch" then
+    return 'de'
+  end
+  if current_language == "Français" then
+    return 'fr-fr'
+  end
+  if current_language == "Italiano" then
+    return 'it'
+  end
+  if current_language == "Polska" then
+    return 'pl'
+  end
+  if current_language == "简体中文" then
+    return 'zh-cn'
+  end
+  if current_language == "日本語" then
+    return 'ja'
+  end
+  if current_language == "한국어" then
+    return 'ko'
+  end
+
+  return 'en'
+end
+
+local settings_text = {
+  spell_editor_shortcut_key = {
+    ui_name = {
+      en = "Spell Editor Shortcut Key",
+      ja = "呪文エディタショートカットキー",
+
+    },
+    ui_description = {
+      en = "Input shortcut key(a-z0-9)",
+      ja = "ショートカットキーを入力してください(a-z0-9)",
+    },
+  },
+  perk_editor_shortcut_key = {
+    ui_name = {
+      en = "Perk Editor Shortcut Key",
+      ja = "呪文エディタショートカットキー",
+    },
+    ui_description = {
+      en = "Input shortcut key(a-z0-9)",
+      ja = "ショートカットキーを入力してください(a-z0-9)",
+    },
+  },
+  show_answers_when_the_game_is_cleared = {
+    ui_name = {
+      en = "The answer will be given when the game is completed",
+      ja = "ゲームクリア時に答え合わせをする",
+    },
+    ui_description = {
+      en = "Hidden spells/perks are forced to be displayed",
+      ja = "隠された呪文と特典が表示されます",
+    },
+  },
+  mystify_spells = {
+    ui_name = {
+      en = "Spell will be made into a mystery",
+      ja = "呪文をシークレットにする",
+    },
+    ui_description = {
+      en = "Mystify the Spells",
+      ja = "呪文のシークレット化",
+    },
+  },
+  mystify_perks = {
+    ui_name = {
+      en = "Park will be made into a mystery",
+      ja = "特典をシークレットにする",
+    },
+    ui_description = {
+      en = "Mystify the Parks",
+      ja = "特典のシークレット化",
+    },
+  },
+  automatics = {
+    ui_name = {
+      en = "Automatic memo",
+      ja = "呪文の登録自動化",
+    },
+    ui_description = {
+      en = "Automatically memos when a spell is triggered",
+      ja = "呪文発射時に呪文の登録を行います",
+    },
+  },
+  automatic_memo_projectile = {
+    ui_name = {
+      en = "Automatic Memo Projectile",
+      ja = "放射物を自動登録する",
+    },
+  },
+  automatic_memo_static_projectile = {
+    ui_name = {
+      en = "Automatic Memo Static Projectile",
+      ja = "静電放射物を自動登録する",
+    },
+  },
+  automatic_memo_modifier = {
+    ui_name = {
+      en = "Automatic Memo Modifier",
+      ja = "調整盤を自動登録する",
+    },
+  },
+  automatic_memo_draw_many = {
+    ui_name = {
+      en = "Automatic Memo Draw many",
+      ja = "マルチキャストを自動登録する",
+    },
+  },
+  automatic_memo_material = {
+    ui_name = {
+      en = "Automatic Memo Material",
+      ja = "資材を自動登録する",
+    },
+  },
+  automatic_memo_other = {
+    ui_name = {
+      en = "Automatic Memo Other",
+      ja = "その他を自動登録する",
+    },
+  },
+  automatic_memo_utility = {
+    ui_name = {
+      en = "Automatic Memo Utility",
+      ja = "ユーティリティーを自動登録する",
+    },
+  },
+  automatic_memo_passive = {
+    ui_name = {
+      en = "Automatic Memo Passive",
+      ja = "受け身を自動登録する",
+    },
+  },
+  automatic_memo_recommended =
+  {
+    ui_name = {
+      en = "==Recommended Setting==\n" ..
+          "Automatic Memo Projectile => On\n" ..
+          "Automatic Memo Static Projectile => On\n" ..
+          "Automatic Memo Modifier => Off\n" ..
+          "Automatic Memo Draw many => Off\n" ..
+          "Automatic Memo Material => Off\n" ..
+          "Automatic Memo Other => Off\n" ..
+          "Automatic Memo Utility => Off\n" ..
+          "Automatic Memo Passive => On\n",
+      ja = "==おすすめの設定==\n" ..
+          "放射物を自動登録するか => オン\n" ..
+          "静電放射物を自動登録するか => オン\n" ..
+          "調整盤を自動登録するか => オフ\n" ..
+          "マルチキャストを自動登録するか => オフ\n" ..
+          "資材を自動登録するか => オフ\n" ..
+          "その他を自動登録するか => オフ\n" ..
+          "ユーティリティを自動登録するか => オフ\n" ..
+          "受け身を自動登録するか => オン\n",
+    }
+  },
+}
+
+
 mod_settings = {
   {
     id = "spell_editor_shortcut_key",
-    ui_name = "Spell Editor Shortcut Key",
-    ui_description = "Input shortcut key",
+    ui_name = settings_text.spell_editor_shortcut_key.ui_name[language()],
+    ui_description = settings_text.spell_editor_shortcut_key.ui_description[language()],
     value_default = "",
     text_max_length = 1,
     allowed_characters = "abcdefghijklmnopqrstuvwxyz0123456789",
@@ -15,8 +190,8 @@ mod_settings = {
   },
   {
     id = "perk_editor_shortcut_key",
-    ui_name = "Perk Editor Shortcut Key",
-    ui_description = "Input shortcut key",
+    ui_name = settings_text.perk_editor_shortcut_key.ui_name[language()],
+    ui_description = settings_text.perk_editor_shortcut_key.ui_description[language()],
     value_default = "",
     text_max_length = 1,
     allowed_characters = "abcdefghijklmnopqrstuvwxyz0123456789",
@@ -24,78 +199,78 @@ mod_settings = {
   },
   {
     id = "show_answers_when_the_game_is_cleared",
-    ui_name = "The answer will be given when the game is completed",
-    ui_description = "Hidden items are forced to be displayed",
+    ui_name = settings_text.show_answers_when_the_game_is_cleared.ui_name[language()],
+    ui_description = settings_text.show_answers_when_the_game_is_cleared.ui_description[language()],
     value_default = true,
     scope = MOD_SETTING_SCOPE_RUNTIME,
   },
   {
     id = "mystify_spells",
-    ui_name = "Spell will be made into a mystery",
-    ui_description = "Mystify the Spells",
+    ui_name = settings_text.mystify_spells.ui_name[language()],
+    ui_description = settings_text.mystify_spells.ui_description[language()],
     value_default = true,
     scope = MOD_SETTING_SCOPE_RUNTIME,
   },
   {
     id = "mystify_perks",
-    ui_name = "Park will be made into a mystery",
-    ui_description = "Mystify the Parks",
+    ui_name = settings_text.mystify_perks.ui_name[language()],
+    ui_description = settings_text.mystify_perks.ui_description[language()],
     value_default = true,
     scope = MOD_SETTING_SCOPE_RUNTIME,
   },
   {
     category_id = "automatics",
-    ui_name = "Automatic memo",
-    ui_description = "Automatically memos when a spell is triggered",
+    ui_name = settings_text.automatics.ui_name[language()],
+    ui_description = settings_text.automatics.ui_description[language()],
     foldable = true,
     _folded = false,
     settings =
     {
       {
         id = "automatic_memo_projectile",
-        ui_name = "Automatic Memo Projectile",
+        ui_name = settings_text.automatic_memo_projectile.ui_name[language()],
         value_default = true,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
       {
         id = "automatic_memo_static_projectile",
-        ui_name = "Automatic Memo Static Projectile",
+        ui_name = settings_text.automatic_memo_static_projectile.ui_name[language()],
         value_default = true,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
       {
         id = "automatic_memo_modifier",
-        ui_name = "Automatic Memo Modifier",
+        ui_name = settings_text.automatic_memo_modifier.ui_name[language()],
         value_default = false,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
       {
         id = "automatic_memo_draw_many",
-        ui_name = "Automatic Memo Draw many",
+        ui_name = settings_text.automatic_memo_draw_many.ui_name[language()],
         value_default = false,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
       {
         id = "automatic_memo_material",
-        ui_name = "Automatic Memo Material",
+        ui_name = settings_text.automatic_memo_material.ui_name[language()],
         value_default = false,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
       {
         id = "automatic_memo_other",
-        ui_name = "Automatic Memo Other",
+        ui_name = settings_text.automatic_memo_other.ui_name[language()],
         value_default = false,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
       {
         id = "automatic_memo_utility",
-        ui_name = "Automatic Memo Utility",
+        ui_name = settings_text.automatic_memo_utility.ui_name[language()],
         value_default = false,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
       {
         id = "automatic_memo_passive",
-        ui_name = "Automatic Memo Passive",
+        ui_name = settings_text.automatic_memo_passive.ui_name[language()],
         value_default = true,
         scope = MOD_SETTING_SCOPE_RUNTIME,
       },
@@ -106,15 +281,7 @@ mod_settings = {
       },
       {
         id = "automatic_memo_recommended",
-        ui_name = "Recommended setting is...\n" ..
-            "Automatic Memo Projectile => On\n" ..
-            "Automatic Memo Static Projectile => On\n" ..
-            "Automatic Memo Modifier => Off\n" ..
-            "Automatic Memo Draw many => Off\n" ..
-            "Automatic Memo Material => Off\n" ..
-            "Automatic Memo Other => Off\n" ..
-            "Automatic Memo Utility => Off\n" ..
-            "Automatic Memo Passive => On\n",
+        ui_name = settings_text.automatic_memo_recommended.ui_name[language()],
         not_setting = true,
       },
     }
